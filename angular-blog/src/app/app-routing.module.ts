412fd73acbde6646cd17a0ffe3e75e8f48a6377d
MatheusDLins <matheus934qwer@gmail.com>
2023-05-04 12:01:23 -0300

    modelo card service pokemon

--- angular-blog/src/app/app-routing.module.ts
+++ angular-blog/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
+  // {
+  //   path:'', component: HomeComponent
+  // },
   {
-    path:'', component: HomeComponent
+    path:'', component: CardComponent
   },
   {
     path:'content/:id', component: ContentComponent
